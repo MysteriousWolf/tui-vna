3e8a870ecd802cb6ef001769f21ad006f9ef8469
MysteriousWolf <matej2000@gmail.com>
2026-03-24 13:10:24 +0100

    Bump version for next minor release

--- src/tina/__init__.py
+++ src/tina/__init__.py
@@ -1,6 +1,6 @@
 """tina - Terminal UI Network Analyzer"""
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 
 from .config.settings import AppSettings, SettingsManager
 from .drivers import HPE5071B as VNA
